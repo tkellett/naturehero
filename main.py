1ded8bfbb6b697f41d77e54fa590c3e27684a4a1
natalia <14nataliia05@gmail.com>
2025-03-08 22:42:51 +0000

    tables added

--- main.py
+++ main.py
@@ -25,6 +25,79 @@ def fetch_times(email, limit):
 
 
 
+
+def create_user(email, name, DoB, tag):
+    key = datastore_client.key("User", email)  # "User" is the kind, email is the unique key
+    
+    # Create a new entity
+    entity = datastore.Entity(key=key)
+
+    # Set entity properties
+    entity.update({
+        "name": name,
+        "dateOfBirth": age,
+        "XP":0
+
+    })
+
+    # Save entity to Datastore
+    datastore_client.put(entity)
+    print(f"User {name} added successfully!")
+
+# Example usage
+create_user("alice@example.com", "Alice", 25)  
+
+
+
+
+
+
+def create_task(email, task_name):
+    user_key = datastore_client.key("User", email)  
+
+    task_key = datastore_client.key("User", email, "Task")  
+
+    # Create the Task entity
+    task = datastore.Entity(key=task_key)
+
+    # Set properties for the Task
+    task.update({
+        "task_name": task_name,
+
+        "status": "To do"
+    })
+
+    # Save the entity
+    datastore_client.put(task)
+    print(f"Task '{task_name}' added for user {email}.")
+
+
+
+def create_personal_task(email, task_name):
+    user_key = datastore_client.key("User", email)  
+    personal_task_key = datastore_client.key("User", email, "Task")  
+
+    # Create the Task entity
+    personal_task = datastore.Entity(key=personal_task_keytask_key)
+
+    # Set properties for the Task
+    personal_task.update({
+        "task_name": task_name,
+        "status": "To do"
+    })
+
+    # Save the entity
+    datastore_client.put(personal_task)
+    print(f"Task '{task_name}' added for user {email}.")
+
+
+
+
+
+
+
+
+
 firebase_request_adapter = requests.Request()
 @app.route("/")
 def root():
